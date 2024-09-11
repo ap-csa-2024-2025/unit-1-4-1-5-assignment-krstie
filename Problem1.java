5dfe7d698ccd8bac59b4e2135112eb5222201f9b
krstie <kristietran010308@gmail.com>
2024-09-11 10:58:48 -0700

    hello

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,13 @@
+public class Problem1
+{
+  public static void main(String[] args)
+  {
+    int intNum = 9;
+    System.out.println("Initial Value: " + intNum);
+    System.out.println("numnber is now " + (intNum++));
+    System.out.println("numnber is now " + (intNum++));
+    System.out.println("numnber is now " + (intNum++));
+    System.out.println("numnber is now " + (intNum++));
+  }
+}
 
