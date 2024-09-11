02bc79c77007a618993725abf7c3e7f149355d6f
krstie <kristietran010308@gmail.com>
2024-09-11 10:58:54 -0700

    hi

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,10 @@
-
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    int value = 293;
+    System.out.println(value % 291);
+    System.out.println(value % 284);
+    System.out.print(value % 290);
+  }
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,18 @@
-
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    int value = 2931;
+    System.out.println("Four digit number:");
+    System.out.println(value);
+    int valFour = (value % 2930);
+    int valThree = (value % 2928);
+    int valTwo = (value % 2922);
+    int valOne =(value % 2929);
+    System.out.println("Here are the digits:");
+    System.out.println(valFour);
+    System.out.println(valThree);
+    System.out.println(valTwo);
+    System.out.println(valOne);
+  }
+}
